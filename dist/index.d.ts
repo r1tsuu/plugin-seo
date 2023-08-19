aa6a9ffb14d4ffee3ce0f70b05351a48b301a17d
r1tsuu <enjoythepain1337@gmail.com>
2023-08-19 18:32:30 +0300

    dist rm

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Config } from 'payload/config';
-import type { PluginConfig } from './types';
-declare const seo: (pluginConfig: PluginConfig) => (config: Config) => Config;
-export default seo;
--- dist/ui/LengthIndicator.d.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-export declare const LengthIndicator: React.FC<{
-    text?: string;
-    minLength?: number;
-    maxLength?: number;
-}>;
